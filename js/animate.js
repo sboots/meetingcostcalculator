abb82e61a4a127991cf3219097ff191457bb1d87
Sean Boots <sean.boots@gmail.com>
2019-10-04 21:01:34 -0400

    Repository clean-up (moved to https://github.com/meetingcostcalculator/meeting-cost-calculator-classic)

--- js/animate.js
+++
@@ -1,8 +0,0 @@
-$.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-});
